56677570b894c721333c1f20c508caa064b3acb1
Ahmed_Emad <7MADAH.1994@Gmail.com>
2014-11-11 23:20:06 +0200

    Initial Commit

---
+++ Assembler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assembler
+//
+//  Created by Ahmed Barakat on 315 / 11 / 14.
+//  Copyright (c) 2014 Ahmed Barakat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
